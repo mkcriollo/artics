c3349b1264b4c68635d5b9f6d90a490a9c68d5dc
Mike Criollo <mkcriollo@gmail.com>
2022-01-25 21:09:36 -0500

    add routes to pages, add navBar and footer to all pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
+import { Navbar, Sidebar, Footer } from "./components";
+import {
+  HomePage,
+  ErrorPage,
+  CartPage,
+  ProductsPage,
+  SingleProductPage,
+  CheckoutPage,
+  AboutPage,
+} from "./pages/index";
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/products/:id" component={SingleProductPage} />
+        <Route path="/products" component={ProductsPage} />
+        <Route path="/cart" component={CartPage} />
+        <Route path="/checkout" component={CheckoutPage} />
+        <Route path="*" component={ErrorPage} />
+      </Switch>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import HomePage from "./HomePage";
+import ErrorPage from "./ErrorPage";
+import AboutPage from "./AboutPage";
+import CartPage from "./CartPage";
+import ProductsPage from "./ProductsPage";
+import SingleProductPage from "./SingleProductPage";
+import CheckoutPage from "./CheckoutPage";
+
+export {
+  HomePage,
+  ErrorPage,
+  CartPage,
+  ProductsPage,
+  SingleProductPage,
+  CheckoutPage,
+  AboutPage,
+};
